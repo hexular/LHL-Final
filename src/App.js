1d1389823bc9d2889b726efae62fe14683e29980
Dan <hexular@gmail.com>
2019-12-19 16:36:06 -0500

    add sample button to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Button></Button> <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
