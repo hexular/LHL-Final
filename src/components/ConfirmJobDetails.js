3881b42835f92d8a9d9a09b35878a8c90ceacb00
Jay <chunsaa@gmail.com>
2019-12-24 17:35:25 -0500

    add stripe transaction feature with test route

--- src/components/ConfirmJobDetails.js
+++ src/components/ConfirmJobDetails.js
@@ -3,11 +3,15 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import { List, ListItem } from 'material-ui/List';
 import RaisedButton from 'material-ui/RaisedButton';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import { STRIPE_KEY } from '../var';
 
 export class ConfirmJobDetails extends Component {
   continue = e => {
     e.preventDefault();
     //add process logic
+    console.log(this)
     this.props.nextStep();
   }
 
@@ -15,9 +19,28 @@ export class ConfirmJobDetails extends Component {
     e.preventDefault();
     this.props.prevStep();
   }
-
+  
   render() {
+    console.log(STRIPE_KEY)
     const { values } = this.props;
+    const jobDetail = this.props;
+    async function handleToken(token) {
+      console.log({token});
+      console.log(values);
+      const response = await axios.post("https://pq0ud.sse.codesandbox.io/checkout", {
+        token,
+        values
+      });
+      const { status } = response.data;
+      console.log("Response:", response.data);
+      if (status === "success") {
+        alert("Success");
+        console.log(jobDetail);
+        jobDetail.nextStep();
+      } else {
+        alert("Something went wrong");
+      }
+    }
     return (
       <MuiThemeProvider>
         <React.Fragment>
@@ -50,6 +73,14 @@ export class ConfirmJobDetails extends Component {
             primary={true}
             style={styles.button}
             onClick={this.continue}
+          />
+          <StripeCheckout
+            stripeKey={ STRIPE_KEY }
+            token={handleToken}
+            amount={ values.requiredTime * values.payRate * 100 }
+            name={ values.serviceType }
+            billingAddress
+            shippingAddress
           />
           <RaisedButton
             label="Back"
