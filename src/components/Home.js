1dbbf4764e44ce575cd8aa43189c2c1cedd4a4ae
Jay <chunsaa@gmail.com>
2019-12-28 01:00:44 -0500

    add material-ui for components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
-import Button from "./Button";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import Button from '@material-ui/core/Button';
 
 export class Home extends Component {
   render(){
     return (
-      <div>
-        <Button name="Log In" onClick={() => this.props.history.push("/login")}></Button>
-        <Button name="Register" onClick={() => this.props.history.push("/register")}></Button>
-      </div>
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="WELCOME! #Lit-Final">
+            <Button color="inherit" onClick={() => this.props.history.push("/login")}>Login</Button>
+            <Button color="inherit" onClick={() => this.props.history.push("/register")}>Register</Button>
+          </AppBar>
+          What do you like better? on nav bar or separated btn?
+          <br/>
+          If we are going mobile view then off nav bar is cleaner
+          <br/>
+          <RaisedButton
+            label="Log In"
+            primary={true}
+            style={styles.button}
+            onClick={() => this.props.history.push("/login")}
+          />
+          <RaisedButton
+            label="Register"
+            primary={true}
+            style={styles.button}
+            onClick={() => this.props.history.push("/register")}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
     );
   }  
 }
 
+const styles = {
+  button: {
+    margin: 15
+  }
+}
+
 export default Home
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,53 @@
-import React from "react";
-import Button from "./Button"
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-export default function Login() {
-
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="email"></input>
-        <input type="text" placeholder="password"></input>
-      </form>
-      <Button name="Login"></Button>
-    </div>
-  );
+export class Login extends Component {
+  render(){
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Login! #Lit-Final"/>
+          <TextField
+            hintText="Email"
+            floatingLabelText="Enter Email"
+          />   
+          <br/>
+          <TextField
+            hintText="Password"
+            floatingLabelText="Enter Password"
+          />   
+          <br/>
+          <RaisedButton
+            label="Log In"
+            primary={true}
+            style={styles.button}
+            onClick={() => 
+              {
+                console.log("perfom get request to server for login");
+                this.props.history.replace("/user");
+              }
+            }
+          />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={() => this.props.history.goBack()}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }  
 }
+
+const styles = {
+  button: {
+    margin: 15
+  }
+}
+
+export default Login
+
