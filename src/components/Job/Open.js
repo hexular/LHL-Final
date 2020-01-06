9d02e599954d001b73f72f2418732c8728853462
Matt Wardle <mwardle.22@gmail.com>
2020-01-06 16:15:08 -0500

    Add button styling

--- src/components/Job/Open.js
+++ src/components/Job/Open.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import Button from "../Button"
+import RaisedButton from "material-ui/RaisedButton"
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default function Open({ job, user, distance, tags }) {
   // TODO: Implement distance calculating here?
 
   return (
-    <li>
-      <h3>{job}</h3>
-      <p>{user}</p>
-      <p>{distance}</p>
-      <ul>{tags}</ul>
-      <Button name="Accept"></Button>
-    </li>
+    <MuiThemeProvider>
+      <li display="flex" flexDirection="row">
+        <h3>{job}</h3>
+        <p>{user}</p>
+        <p>{distance}</p>
+        <ul>{tags}</ul>
+        <RaisedButton
+          label="Accept"
+          primary={true}
+        />
+      </li>
+    </MuiThemeProvider>
   )
 }
 
