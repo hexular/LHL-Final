5b46744d964841a1eee0f8c359ece5d2e82c6aa6
Matt Wardle <mwardle.22@gmail.com>
2020-01-06 19:14:23 -0500

    Finish updating all login/signup pages

--- src/components/JobberLogin.js
+++ src/components/JobberLogin.js
@@ -4,14 +4,10 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu'
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
@@ -113,10 +109,6 @@ export default function JobberLogin() {
                   autoComplete="current-password"
                   onChange={(event) => setPassword(event.target.value)}
                 />
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
                 <Button
                   type="button"
                   fullWidth
@@ -127,18 +119,9 @@ export default function JobberLogin() {
                 >
                   Sign In
               </Button>
-                <Grid container>
-                  {/* <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid> */}
-                  <Grid item>
-                    <Link href="#" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
+                <Link href="/jobbersignup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
               </form>
             </div>
           </Container>
@@ -147,5 +130,4 @@ export default function JobberLogin() {
   );
 }
 
-// export default JobberLogin
 
--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,75 +1,132 @@
-import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import axios from 'axios';
+import { Redirect } from 'react-router';
 
-export class UserLogin extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
 
-  handleChange = input => e => {
-    this.setState({[input]: e.target.value});
-  }
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  submit = () => {
-    console.log(this);
-    const parent = this;
+export default function UserLogin() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [submitted, setSubmitted] = useState(false)
+
+  const submit = () => {
     const loginInfo = {
-      email: this.state.email.trim().toLowerCase(),
-      password: this.state.password
+      email: email.trim().toLowerCase(),
+      password: password
     }
     axios.post('/auth/login', loginInfo)
-    .then(function (response) {
-      response.data.result ? parent.props.history.replace("/user") : alert("account does not exist or invalid email/pw");
-      console.log(response);
-    });
+      .then(function (response) {
+        if (response.data.result) {
+          setSubmitted(true)
+        } else {
+          alert("account does not exist or invalid email/pw");
+        }
+      })
+      .catch(
+        console.log("error")
+      );
   }
 
-  render(){
-    return (
+  return (
+    submitted ? <Redirect to="/user" /> :
       <MuiThemeProvider>
         <React.Fragment>
-          <AppBar title="Customer Login! #Lit-Final"/>
-          <TextField
-            hintText="Email"
-            floatingLabelText="Enter Email"
-            onChange={this.handleChange('email')}
-          />   
-          <br/>
-          <TextField
-            type="password"
-            hintText="Password"
-            floatingLabelText="Enter Password"
-            onChange={this.handleChange('password')}
-          />
-          <br/>
-          <RaisedButton
-            label="Log In"
-            primary={true}
-            style={styles.button}
-            onClick={this.submit}
-          />
-          <RaisedButton
-            label="Back"
-            primary={false}
-            style={styles.button}
-            onClick={() => this.props.history.goBack()}
-          />
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                User Login
+            </Typography>
+            </Toolbar>
+          </AppBar>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+              <form className={classes.form} noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={submit}
+                >
+                  Sign In
+              </Button>
+                <Link href="/usersignup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </form>
+            </div>
+          </Container>
         </React.Fragment>
       </MuiThemeProvider>
-    );
-  }  
+  );
 }
 
-const styles = {
-  button: {
-    margin: 15
-  }
-}
-
-export default UserLogin
 
