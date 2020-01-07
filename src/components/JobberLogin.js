bd712b3ee865b15d75c8f1d7cf3dec05bdd1eacc
Dan <hexular@gmail.com>
2020-01-07 17:13:14 -0500

    fix merge conflict

--- src/components/JobberLogin.js
+++ src/components/JobberLogin.js
@@ -1,9 +1,6 @@
 
 import React, { Component, useState } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from './Appbar';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
