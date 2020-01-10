e351fdcb49348ea84414f7c18d823bbdc736595e
Jay <chunsaa@gmail.com>
2020-01-10 14:38:16 -0500

    change api key

--- db/schema/05_jobber_skills.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS jobber_skills CASCADE;
-
-CREATE TABLE jobber_skills CASCADE (
-  id SERIAL PRIMARY KEY NOT NULL,
-  jobber_id INTEGER REFERENCES jobbers(id),
-  skill_id INTEGER REFERENCES skills(id),
-);
