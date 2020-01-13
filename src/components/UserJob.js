671b68f58635d5011d01addb1331d5383a0f7ed1
Jay <chunsaa@gmail.com>
2020-01-13 13:14:59 -0500

    fix delete for socket

--- src/components/UserJob.js
+++ src/components/UserJob.js
@@ -44,6 +44,7 @@ export default function UserJob(props) {
                 axios.put(`/myjobs`, [props.job.id], {withCredentials: true})
                 // props.finished()
                 props.updateMyJobs()
+                props.updateAllJobs()
               }
               }
               primary={false}
